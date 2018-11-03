69e7696ba9298990c4d8ed71dd7d42c60d23110e
DESKTOP-3SE1LP0\Mike <whitaker.mj@gmail.com>
2018-11-03 09:30:46 +0100

    initial filling of repo and made intial state

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name formValidateDirectiveApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the formValidateDirectiveApp
+ */
+angular.module('formValidateDirectiveApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
