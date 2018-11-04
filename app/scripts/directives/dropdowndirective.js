71ece46e93e91893cba9d0a4e0081a8f56538276
DESKTOP-3SE1LP0\Mike <whitaker.mj@gmail.com>
2018-11-04 09:59:22 +0100

    still not working example from course

--- app/scripts/directives/dropdowndirective.js
+++ app/scripts/directives/dropdowndirective.js
@@ -8,10 +8,28 @@
  */
 angular.module("formValidateDirectiveApp").directive("dropdown", function() {
   return {
+    require: ["dropdown", "ngModel"],
     restrict: "E",
-    controller: function($scope, $attrs) {
-      $scope.requiredParamExists = $attrs.hasOwnProperty( 'requiredparam' );
+    scope: {
     },
+    controller: function($scope, $attrs) {
+      var vm = this;
+      var ngModel;
+      
+      vm.init = function (ngModelController) {
+        ngModel = ngModelController;
+        $scope.modelValue = ngModel;
+      };
+
+
+      $scope.requiredParamExists = $attrs.hasOwnProperty( 'requireValidation' );
+    },
+    link: function(scope, element, attributes, controllers){
+      var dropDownController = controllers[0];
+      var ngModelController = controllers[1];
+      dropDownController.init(ngModelController)
+    },
+    controllerAs: "dropdownCtrl",
     templateUrl: "/scripts/directives/dropdown.html"
   };
 });
